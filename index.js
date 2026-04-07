765af1851c8f612764a0f7ccbec7bea97f8a2d84
samir07-code <samir100100306@gmail.com>
2026-04-06 21:19:27 -0500

    Added more project info

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 const projects = [
     {
-        name: "fg",
-        description: "WIP - Getting data", // TODO: get data
-        link: "",
+        name: "Fidget Game",
+        description: "One of my first projects, a simple game which garnered 50,000 plays on Roblox.", // TODO: get data
+        link: "https://www.roblox.com/games/6596879170/Fidget-Game-Beta#!/about",
         time: [[new Date("2020-12-01"), new Date("2021-07-01")]],
         tags: ["lua", "game-design", "ui", "video-demo"]
     },
     { // TODO: find more of my python macros
         name: "Python Autoclicker",
-        description: "",
-        link: "",
+        description: "A simple Python script that automates mouse clicks, useful for repetitive tasks or games.",
+        link: "https://github.com/this-salami/python-macros/blob/main/AutoClick.py",
         time: [[new Date("2022-06-01")]],
         tags: ["python", "open-source", "video-demo"] // TODO: add demo
     },
     {
         name: "Python Piano Tiles Bot",
-        description: "",
-        link: "",
+        description: "A bot that plays the popular Piano Tiles game automatically using Python.",
+        link: "https://github.com/this-salami/python-macros/blob/main/MusicalTiles.py",
         time: [[new Date("2023-02-01")]],
         tags: ["python", "open-source", "video-demo"] // TODO: add demo
     },
     {
         name: "Minesweeper Bookmarklet",
-        description: "", // TODO: add data
+        description: "A bookmarklet that allows you to solve minesweeper puzzles automatically.",
         link: "https://github.com/this-salami/minesweeper-bookmarklet",
         time: [[new Date("2023-02-01"), new Date("2023-04-01")]], // TODO: Find out exact dates
         tags: ["js", "open-source", "video-demo"] // TODO: add demo
     },
     {
         name: "Isometric Map Test",
-        description: "", // TODO: add data
+        description: "Isometric map test inspired by classic isometric games, showcasing tile-based rendering and movement.", // TODO: add data
         link: "https://github.com/this-salami/Isometric-Map-Test-2025",
         time: [[new Date("2023-09-01"), new Date("2023-11-01")]], // TODO: Find out exact dates
         tags: ["js", "open-source", "video-demo"] // TODO: add demo
@@ -43,28 +43,28 @@ const projects = [
     },
     {
         name: "Button Outline Demo",
-        description: "", // TODO: add data
+        description: "A small animation demo showcasing button outline effects using CSS and JavaScript, inspired by Scrimba button animation.", // TODO: add data
         link: "https://github.com/this-salami/button-outline-demo",
         time: [[new Date("2022-02-01"), new Date("2022-03-01")]], // TODO: Find out exact dates
         tags: ["js", "open-source", "video-demo"] // TODO: add demo
     },
     {
         name: "Web Based Command Line",
-        description: "", // TODO: add data
+        description: "Very basic web-based command line interface for executing commands in a simulated environment, has no functionality, just design for now.", // TODO: add data
         link: "https://github.com/this-salami/cmd-line-2025",
         time: [[new Date("2024-02-01"), new Date("2024-04-01")]], // TODO: Find out exact dates
         tags: ["js", "open-source", "interactive-demo"] // TODO: add demo
     },
     {
         name: "A* Pathfinding Demo",
-        description: "", // TODO: add data
+        description: "A demonstration of the A* pathfinding algorithm, showcasing its ability to find the shortest path in a grid-based environment.", // TODO: add data
         link: "https://github.com/this-salami/A-star-pathfinding-2025-demo",
         time: [[new Date("2024-04-01"), new Date("2024-06-01")]], // TODO: Find out exact dates
         tags: ["js", "open-source", "interactive-demo"] // TODO: add demo
     },
     {
         name: "3D Card Showcase",
-        description: "", // TODO: add data
+        description: "A small project displaying a card as a 3-d element with JS and CSS.", // TODO: add data
         link: "https://github.com/this-salami/3d-card-showcase",
         time: [[new Date("2021-12-01")]], // TODO: Find out exact dates
         tags: ["js", "open-source", "interactive-demo"] // TODO: add demo
