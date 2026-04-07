e7f3f37df931338fbafb3d8a785c5d16d70b96a3
samir07-code <samir100100306@gmail.com>
2026-04-06 22:32:49 -0500

    Moved project data to seperate file

---
+++ projects.js
@@ -0,0 +1,163 @@
+export const projects = [
+    {
+        name: "Fidget Game",
+        description: "One of my first projects, a simple game which garnered 50,000 plays on Roblox.", // TODO: get data
+        link: "https://www.roblox.com/games/6596879170/Fidget-Game-Beta#!/about",
+        time: [[new Date("2020-12-01"), new Date("2021-07-01")]],
+        tags: ["lua", "game-design", "ui", "video-demo"]
+    },
+    { // TODO: find more of my python macros
+        name: "Python Autoclicker",
+        description: "A simple Python script that automates mouse clicks.",
+        link: "https://github.com/this-salami/python-macros/blob/main/AutoClick.py",
+        time: [[new Date("2022-06-01")]],
+        tags: ["python", "open-source", "video-demo"] // TODO: add demo
+    },
+    {
+        name: "Python Piano Tiles Bot",
+        description: "A bot that plays the popular Piano Tiles game automatically using Python.",
+        link: "https://github.com/this-salami/python-macros/blob/main/MusicalTiles.py",
+        time: [[new Date("2023-02-01")]],
+        tags: ["python", "open-source", "video-demo"] // TODO: add demo
+    },
+    {
+        name: "Minesweeper Bookmarklet",
+        description: "A bookmarklet that allows you to solve minesweeper puzzles automatically.",
+        link: "https://github.com/this-salami/minesweeper-bookmarklet",
+        time: [[new Date("2023-02-01"), new Date("2023-04-01")]], // TODO: Find out exact dates
+        tags: ["js", "open-source", "video-demo"] // TODO: add demo
+    },
+    {
+        name: "Isometric Map Test",
+        description: "Isometric map test inspired by classic isometric games, showcasing tile-based rendering and movement.", // TODO: add data
+        link: "https://github.com/this-salami/Isometric-Map-Test-2025",
+        time: [[new Date("2023-09-01"), new Date("2023-11-01")]], // TODO: Find out exact dates
+        tags: ["js", "open-source", "video-demo"] // TODO: add demo
+    },
+    {
+        name: "Chess.com Python API",
+        description: "Assignment in Highschool coding class, uses Chess.com API to fetch player data.",
+        link: "", // TODO: add link
+        time: [[new Date("2024-09-01")]], 
+        tags: ["python", "open-source", "video-demo"] // TODO: add demo
+    },
+    {
+        name: "Python Wordle Game",
+        description: "Assignment in Highschool coding class, where I implemented Wordle in Python.",
+        link: "", // TODO: add link
+        time: [[new Date("2024-09-01"), new Date("2024-11-01")]], 
+        tags: ["python", "open-source", "video-demo", "ui"] // TODO: add demo
+    },
+    {
+        name: "Python Chess Game",
+        description: "Assignment in Highschool coding class, the game of chess implemented in Python.",
+        link: "", // TODO: add link
+        time: [[new Date("2025-01-01"), new Date("2025-04-01")]], 
+        tags: ["python", "open-source", "video-demo", "ui"] // TODO: add demo
+    },
+    {
+        name: "Button Outline Demo",
+        description: "A small animation demo showcasing button outline effects using CSS and JavaScript, inspired by Scrimba button animation.", // TODO: add data
+        link: "https://github.com/this-salami/button-outline-demo",
+        time: [[new Date("2022-02-01"), new Date("2022-03-01")]], // TODO: Find out exact dates
+        tags: ["js", "css", "open-source", "video-demo"] // TODO: add demo
+    },
+    {
+        name: "Web Based Command Line",
+        description: "Very basic web-based command line interface for executing commands in a simulated environment, has no functionality, just design for now.", // TODO: add data
+        link: "https://github.com/this-salami/cmd-line-2025",
+        time: [[new Date("2024-02-01"), new Date("2024-04-01")]], // TODO: Find out exact dates
+        tags: ["js", "css", "open-source", "interactive-demo"] // TODO: add demo
+    },
+    {
+        name: "A* Pathfinding Demo",
+        description: "A demonstration of the A* pathfinding algorithm, showcasing its ability to find the shortest path in a grid-based environment.", // TODO: add data
+        link: "https://github.com/this-salami/A-star-pathfinding-2025-demo",
+        time: [[new Date("2024-04-01"), new Date("2024-06-01")]], // TODO: Find out exact dates
+        tags: ["js", "css", "open-source", "interactive-demo"] // TODO: add demo
+    },
+    {
+        name: "3D Card Showcase",
+        description: "A small project displaying a card as a 3-d element with JS and CSS.", // TODO: add data
+        link: "https://github.com/this-salami/3d-card-showcase",
+        time: [[new Date("2021-12-01")]], // TODO: Find out exact dates
+        tags: ["js", "css", "open-source", "interactive-demo"] // TODO: add demo
+    },
+    { // TODO: finish/find
+        name: "dsa",
+        description: "",
+        link: "",
+        time: [],
+        tags: ["js", "html", "css", "ui", "front-end", "open-source", "interactive-demo"]
+    },
+    { // TODO: find
+        name: "es",
+        description: "",
+        link: "",
+        time: [],
+        tags: ["python", "ui", "open-source", "video-demo"]
+    },
+    { // TODO: finish/find
+        name: "egep",
+        description: "",
+        link: "",
+        time: [],
+        tags: ["python", "ui", "open-source"] // bro finish ts
+    },
+    { // TODO: finish/find
+        name: "lg",
+        description: "",
+        link: "",
+        time: [],
+        tags: ["js", "html", "css", "ui", "front-end", "open-source"] // server TODO: figure out
+    },
+    {
+        name: "Read Fast",
+        description: "A short project I made to test out a speed reading method called Spritz. It was pretty fun to make, and my friends and I use it on occasion.", 
+        link: "https://github.com/this-salami/read-fast",
+        time: [[new Date("2025-10-01")]],
+        tags: ["js", "html", "css", "open-source", "interactive-demo"]
+    },
+    { // TODO: finish
+        name: "Degree Planner (WIP)",
+        description: "A WIP project that I started to aid in class selection and degree matching. Uses a webscrape to check for class requirements and generates a schedule based on the classes you want to take.",
+        link: "https://github.com/this-salami/class-selector",
+        time: [[new Date("2026-01-01"), new Date("2026-04-01")]],
+        tags: ["js", "html", "css", "php", "react", "front-end", "open-source", "interactive-demo"]
+    },
+    { // TODO: find
+        name: "db",
+        description: "",
+        link: "",
+        time: [], // maybe idk
+        tags: ["python"] // maybe back-end TODO: idk
+    },
+    { // TODO: finish/find
+        name: "bnm",
+        description: "",
+        link: "",
+        time: [], // bro finish ts
+        tags: ["js", "html", "css", "ui", "front-end", "open-source"]
+    },
+    { // TODO: finish/find
+        name: "msmc",
+        description: "",
+        link: "",
+        time: [], // bro finish ts
+        tags: ["js", "html", "css", "ui", "front-end"]
+    },
+    { // TODO: finish/find
+        name: "wds",
+        description: "",
+        link: "",
+        time: [], // bro finish ts
+        tags: ["js", "html", "css", "ui", "front-end"]
+    },
+    { // TODO: finish/find
+        name: "ts",
+        description: "",
+        link: "",
+        time: [], // bro you aint even finishing ts
+        tags: ["js", "html", "css", "ui", "front-end"]
+    }
+];
